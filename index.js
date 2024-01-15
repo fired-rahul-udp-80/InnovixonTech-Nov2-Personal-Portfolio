cc67b041e724ab45a298d3c8cc034dfd0dfb4dea
fired-rahul-udp-80 <152783083+fired-rahul-udp-80@users.noreply.github.com>
2024-01-16 00:04:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+ 
+const popUp1 = document.querySelector(".pop-up1");
+
+
+function popUpDiv(){
+     setInterval(()=>{
+          popUp1.style.visibility ="hidden";
+     },4000);
+      
+
+     
+}
+popUpDiv();
+
+ 
