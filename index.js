f1c57813cdea3725d8058cb62d9a8a7f705e0f38
fired-rahul-udp-80 <152783083+fired-rahul-udp-80@users.noreply.github.com>
2024-01-16 14:32:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const popUp1 = document.querySelector(".pop-up1");
 function popUpDiv(){
      setInterval(()=>{
           popUp1.style.visibility ="hidden";
-     },4000);
+     },3400);
       
 
      
